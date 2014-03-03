fd75cb4f8f356da7ce59232441443e9b5f6b10be
sekka1 <garlandk@gmail.com>
2014-03-02 22:54:02 -0800

    updated live stream with system_provisioning messages

--- assets/js/AutoDevBot/liveEventStreamPage.js
+++ assets/js/AutoDevBot/liveEventStreamPage.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     // Connect to the socket.io interface
     var socket = io.connect('http://localhost:8080');
 
-    socket.emit('subscribe-event-stream', {user_id:'b9e45b2320a544b8b017fbf60fb04247', authToken:'5502ebcd7ba71584964731f1a34197c5'});
+    socket.emit('subscribe-event-stream', {user_id:'c12c9bb5615743e1a4f3633a2dba620d', authToken:'5502ebcd7ba71584964731f1a34197c5'});
 
     socket.on('subscribe-event-stream', function (data) {
         //console.log(data);
@@ -11,13 +11,18 @@ $( document ).ready(function() {
 
         console.log('user_id: '+event.user_id);
 
-        if(event.testStatus.toLowerCase() == 'success'){
-            // Success row
-            $('#tableEvents').prepend('<tr><td>'+event.queue_event_id.substring(0,10)+'</td><td>'+event.eventType+'</td><td>'+event.createdOn+'</td><td><span class="label label-success">'+event.testStatus+'</span></td><td>'+event.testResultJSON.testsuites.testsuite[0].$.name+'</td><td>-</td></tr>');
-        }
-        if(event.testStatus.toLowerCase() == 'failure'){
-            // Failure row
-            $('#tableEvents').prepend('<tr><td>'+event.queue_event_id.substring(0,10)+'</td><td>'+event.eventType+'</td><td>'+event.createdOn+'</td><td><span class="label label-danger">'+event.testStatus+'</span></td><td>'+event.testResultJSON.testsuites.testsuite[0].$.name+'</td><td>'+event.testResultJSON.testsuites.testsuite[0].testcase[0].failure+'</td></tr>');
+        if(event.eventType.toLowerCase() == 'monitoring_frisbyjs'){
+
+            if(event.testStatus.toLowerCase() == 'success'){
+                // Success row
+                $('#tableEvents').prepend('<tr><td>'+event.queue_event_id.substring(0,10)+'</td><td>'+event.eventType+'</td><td>'+event.createdOn+'</td><td><span class="label label-success">'+event.testStatus+'</span></td><td>'+event.testResultJSON.testsuites.testsuite[0].$.name+'</td><td>-</td></tr>');
+            }
+            if(event.testStatus.toLowerCase() == 'failure'){
+                // Failure row
+                $('#tableEvents').prepend('<tr><td>'+event.queue_event_id.substring(0,10)+'</td><td>'+event.eventType+'</td><td>'+event.createdOn+'</td><td><span class="label label-danger">'+event.testStatus+'</span></td><td>'+event.testResultJSON.testsuites.testsuite[0].$.name+'</td><td>'+event.testResultJSON.testsuites.testsuite[0].testcase[0].failure+'</td></tr>');
+            }
+        }else if(event.eventType.toLowerCase() == 'system_provisioning'){
+            $('#tableEvents').prepend('<tr><td>-</td><td>'+event.eventType+'</td><td>'+event.createdOn+'</td><td><span class="label label-default">'+event.status+'</span></td><td>'+event.item+'</td><td>'+event.description+'</td></tr>');
         }
 
         // Prune table
