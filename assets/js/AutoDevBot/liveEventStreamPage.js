dbb17da4dea07c8f652a55e43dba0f2009a0d135
sekka1 <garlandk@gmail.com>
2014-03-04 23:34:02 -0800

    updated live dashboard with new urls

--- assets/js/AutoDevBot/liveEventStreamPage.js
+++ assets/js/AutoDevBot/liveEventStreamPage.js
@@ -1,9 +1,36 @@
 $( document ).ready(function() {
 
-    // Connect to the socket.io interface
-    var socket = io.connect('http://localhost:8080');
+    var QueryString = function () {
+        // This function is anonymous, is executed immediately and
+        // the return value is assigned to QueryString!
+        var query_string = {};
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            // If first entry with this name
+            if (typeof query_string[pair[0]] === "undefined") {
+                query_string[pair[0]] = pair[1];
+                // If second entry with this name
+            } else if (typeof query_string[pair[0]] === "string") {
+                var arr = [ query_string[pair[0]], pair[1] ];
+                query_string[pair[0]] = arr;
+                // If third or later entry with this name
+            } else {
+                query_string[pair[0]].push(pair[1]);
+            }
+        }
+        return query_string;
+    } ();
 
-    socket.emit('subscribe-event-stream', {user_id:'c12c9bb5615743e1a4f3633a2dba620d', authToken:'5502ebcd7ba71584964731f1a34197c5'});
+    console.log(QueryString.user_id);
+    console.log(QueryString.temp_key);
+
+    // Connect to the socket.io interface
+    var socket = io.connect('https://api.autodevbot.com');
+
+    //socket.emit('subscribe-event-stream', {user_id:'b9e45b2320a544b8b017fbf60fb04247', authToken:'5502ebcd7ba71584964731f1a34197c5'});
+    socket.emit('subscribe-event-stream', {user_id: QueryString.user_id, authToken: QueryString.temp_key});
 
     socket.on('subscribe-event-stream', function (data) {
         //console.log(data);
@@ -30,5 +57,4 @@ $( document ).ready(function() {
         console.log('rowCount: '+rowCount);
         $("#tableEvents tr:eq(20)").remove();
     });
-
 });
